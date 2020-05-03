f78bc83aa9b186a97c3b410888c5785ba9e545ea
MayronGMZ <mayron.souza@gmz.solutions>
2020-05-03 11:19:05 -0300

    Primeiro commit do Chat

---
+++ client/src/components/Join.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Join = () => (
+  <h1>I am Join.</h1>
+)
+
+export default Join
---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log('We have a new connection!!!')
+
+  socket.on('desconnect', () => console.log('User had left!!!'))
+})
+
+app.use(routes)
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}.`))
